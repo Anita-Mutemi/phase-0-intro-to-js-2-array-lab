1e1d0e05efec13e5d8d899f7b0f6be5c5caec3af
Anita-Mutemi <anita.mutemi@student.moringaschool.com>
2023-06-15 15:35:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+];
+
+function destructivelyAppendCat(bimbo) {
+  cats.push(bimbo);
+}
+
+function destructivelyPrependCat(bimbo) {
+  cats.unshift(bimbo);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+}
+
+function appendCat(bimbo) {
+  const newCats = cats.slice();
+  newCats.push(bimbo);
+  return newCats;
+}
+
+function prependCat(bimbo) {
+  const newCats = cats.slice();
+  newCats.unshift(bimbo);
+  return newCats;
+}
+
+function removeLastCat() {
+  const newCats = cats.slice();
+  newCats.pop();
+  return newCats;
+}
+
+function removeFirstCat() {
+  const newCats = cats.slice();
+  newCats.shift();
+  return newCats;
+}
+
